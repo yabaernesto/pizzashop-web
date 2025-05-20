7e1ce43938097a3fbf178396e4cdbe3831094360
Yaba Ernesto <yabaernesto@gmail.com>
2025-05-20 16:26:16 +0100

    feat: Gráfico de receita no período adicionado

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -3,6 +3,7 @@ import { MothRevenueCard } from './moth-revenue-card';
 import { MothOrdersAmountCard } from './moth-orders-amount-card';
 import { DayOrdersAmountCard } from './day-orders-amount-card';
 import { MothCanceledOrdersAmountCard } from './moth-canceled-orders-amount-card';
+import { RevenueChart } from './revenue-chart';
 
 export function Dashboard() {
   return (
@@ -16,6 +17,10 @@ export function Dashboard() {
           <MothOrdersAmountCard />
           <DayOrdersAmountCard />
           <MothCanceledOrdersAmountCard />
+        </div>
+
+        <div className="grid grid-cols-9 gap-4">
+          <RevenueChart />
         </div>
       </div>
     </>
