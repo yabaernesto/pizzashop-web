3aac4d760540e677c251164bf1b5898d4e2f0106
Yaba Ernesto <yabaernesto@gmail.com>
2025-05-20 17:11:24 +0100

    feat: Pagina 404 adicionada

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,11 +6,13 @@ import { SignIn } from './pages/auth/sign-in';
 import { SignUp } from './pages/auth/sign-up';
 import { Orders } from './pages/app/orders/orders';
 import { Dashboard } from './pages/app/dashboard/dashboard';
+import { NotFound } from './pages/404';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/orders', element: <Orders /> },
