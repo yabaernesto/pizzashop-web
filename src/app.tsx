0cb56e3de39b2fc93d2daaa36c4f63a1f91c9c27
Yaba Ernesto <yabaernesto@gmail.com>
2025-10-21 09:38:39 +0100

    chore: config tanstack query, react query

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,4 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
@@ -7,13 +8,17 @@ import { router } from './routes'
 import './index.css'
 import { ThemeProvider } from './components/theme/theme-provider'
 
+import { queryClient } from './lib/react-query'
+
 export function App() {
   return (
     <HelmetProvider>
       <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
         <Helmet titleTemplate="%s | pizza.shop" />
         <Toaster richColors />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
