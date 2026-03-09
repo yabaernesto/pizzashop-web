cccf43680e5de3c55a938cbf07a1b77bca4998c1
Yaba Ernesto <yabaernesto@gmail.com>
2026-03-09 01:15:03 +0100

    chore: tailwindcss, shadcn-ui, eslint e prettier adicionados

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
+import path from "node:path";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.basename(__dirname, "./src"),
+    },
+  },
 });
