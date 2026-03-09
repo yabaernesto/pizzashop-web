987d140a62e4cd3d825b40917302577f97c469b8
Yaba Ernesto <yabaernesto@gmail.com>
2026-03-09 01:41:37 +0100

    chore: auto layouts adicionados

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,8 +2,16 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { Dashboard } from './pages/dashboard'
 import { SignIn } from './pages/auth/sign-in'
+import { AppLayout } from './pages/_layout/app'
+import { AuthLayout } from './pages/_layout/auth'
 
 export const router = createBrowserRouter([
-  { path: '/', element: <Dashboard /> },
-  { path: '/sign-in', element: <SignIn /> }
+  { 
+    path: '/', element: <AppLayout />,
+    children: [{ path: '/', element: <Dashboard />}]
+  },
+  { 
+    path: '/', element: <AuthLayout />,
+    children: [{ path: '/sign-in', element: <SignIn /> }]
+  },
 ])
