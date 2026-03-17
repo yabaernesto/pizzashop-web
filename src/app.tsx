3f82e23bfb97ca854b6bae450b51dbba815ae746
Yaba Ernesto <yabaernesto@gmail.com>
2026-03-18 00:36:54 +0100

    feat: sonner adicionado

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 import { HelmetProvider, Helmet } from 'react-helmet-async'
 
 import { router } from './routes'
@@ -7,6 +8,7 @@ export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate='%s | pizza.shop'/>
+      <Toaster richColors />
       <RouterProvider router={router} />
     </HelmetProvider>
   )
