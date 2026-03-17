4e53b1828b676b21a64c50b81c15e9f862a3f6a6
Yaba Ernesto <yabaernesto@gmail.com>
2026-03-18 00:37:09 +0100

    feat: rota sign-up adicionada

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { Dashboard } from './pages/dashboard/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 import { AppLayout } from './pages/_layout/app'
 import { AuthLayout } from './pages/_layout/auth'
+import { SignUp } from './pages/auth/sign-up'
 
 export const router = createBrowserRouter([
   { 
@@ -12,6 +13,9 @@ export const router = createBrowserRouter([
   },
   { 
     path: '/', element: <AuthLayout />,
-    children: [{ path: '/sign-in', element: <SignIn /> }]
+    children: [
+      { path: '/sign-in', element: <SignIn /> },
+      { path: '/sign-up', element: <SignUp /> },
+    ]
   },
 ])
