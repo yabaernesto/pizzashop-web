4c9c4e0e96c621ee2c2a8fc5a2f46cd30944d855
Yaba Ernesto <yabaernesto@gmail.com>
2026-03-18 04:07:21 +0100

    feat: componente filtro e pedidos

---
+++ src/pages/dashboard/orders/components/order-table-row.tsx
@@ -0,0 +1,47 @@
+import { ArrowRight, Search, X } from 'lucide-react'
+
+import { TableCell, TableRow } from '@/components/ui/table'
+import { Button } from '@/components/ui/button'
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <Search className='h-3 w-3' />
+          <span className="sr-only">Detalhes do pedido</span>
+        </Button>
+      </TableCell>
+      <TableCell className='text-xs font-mono font-medium'>
+        sa182e1ujdx1298dj
+      </TableCell>
+      <TableCell className='text-muted-foreground'>
+        há 15 minutos
+      </TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="w-2 h-2 rounded-full bg-slate-400" />
+          <span className="font-medium text-muted-foreground">
+            Pendente
+          </span>
+        </div>
+      </TableCell>
+      <TableCell className='font-medium'>
+        Yaba S. M. Ernesto
+      </TableCell>
+      <TableCell className='font-medium'>R$ 149,90</TableCell>
+      <TableCell>
+        <Button variant="outline" className='cursor-pointer'>
+          <ArrowRight className='h-3 w-3 mr-2' />
+            Aprovar
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant="ghost" className='cursor-pointer'>
+          <X className='h-3 w-3 mr-2' />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
--- src/pages/dashboard/orders/orders.tsx
+++ src/pages/dashboard/orders/orders.tsx
@@ -1,16 +1,14 @@
-import { ArrowRight, Search, X } from 'lucide-react'
 import { Helmet } from 'react-helmet-async'
 
-import { Input } from '@/components/ui/input'
 import { 
   Table, 
-  TableBody, 
-  TableCell, 
+  TableBody,
   TableHead, 
   TableHeader, 
   TableRow 
 } from '@/components/ui/table'
-import { Button } from '@/components/ui/button'
+import { OrderTableRow } from './components/order-table-row'
+import { OrderTableFilters } from './components/order-table-filters'
 
 export function Orders() {
   return (
@@ -21,12 +19,7 @@ export function Orders() {
       </div>
 
       <div className="space-y-2.5">
-        <form className="flex items-center gap-2">
-          <span className="text-sm font-semibold">
-            Filtros:
-          </span>
-          <Input placeholder='Nome do cliente' className='h-8 w-[320px]' />
-        </form>
+        <OrderTableFilters />
 
         <div className="border rounded-md">
           <Table>
@@ -45,44 +38,7 @@ export function Orders() {
             <TableBody>
               {Array.from({ length: 10 }).map((_, index) => {
                 return (
-                  <TableRow key={index}>
-                    <TableCell>
-                      <Button variant="outline" size="xs">
-                        <Search className='h-3 w-3' />
-                        <span className="sr-only">Detalhes do pedido</span>
-                      </Button>
-                    </TableCell>
-                    <TableCell className='text-xs font-mono font-medium'>
-                      sa182e1ujdx1298dj
-                    </TableCell>
-                    <TableCell className='text-muted-foreground'>
-                      há 15 minutos
-                    </TableCell>
-                    <TableCell>
-                      <div className="flex items-center gap-2">
-                        <span className="w-2 h-2 rounded-full bg-slate-400" />
-                        <span className="font-medium text-muted-foreground">
-                          Pendente
-                        </span>
-                      </div>
-                    </TableCell>
-                    <TableCell className='font-medium'>
-                      Yaba S. M. Ernesto
-                    </TableCell>
-                    <TableCell className='font-medium'>R$ 149,90</TableCell>
-                    <TableCell>
-                      <Button variant="outline" className='cursor-pointer'>
-                        <ArrowRight className='h-3 w-3 mr-2' />
-                        Aprovar
-                      </Button>
-                    </TableCell>
-                    <TableCell>
-                      <Button variant="ghost" className='cursor-pointer'>
-                        <X className='h-3 w-3 mr-2' />
-                        Cancelar
-                      </Button>
-                    </TableCell>
-                  </TableRow>
+                  <OrderTableRow key={index} />
                 )
               })}
             </TableBody>
