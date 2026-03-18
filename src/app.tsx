6c4d206a9bb41bd25c529ea5898b54450e149f93
Yaba Ernesto <yabaernesto@gmail.com>
2026-03-18 02:03:09 +0100

    feat: header e menu toggle

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,19 @@
-import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
+import { RouterProvider } from 'react-router-dom'
 import { HelmetProvider, Helmet } from 'react-helmet-async'
+
+import { ThemeProvider } from '@/components/theme/theme-provider'
 
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate='%s | pizza.shop'/>
-      <Toaster richColors />
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme='dark' storageKey='pizzashop-theme'>
+        <Helmet titleTemplate='%s | pizza.shop'/>
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
