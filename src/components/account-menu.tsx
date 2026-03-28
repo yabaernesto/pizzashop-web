e3959767a5108c12f856c704ab9c2da521bc7d3e
Yaba Ernesto <yabaernesto@gmail.com>
2026-03-28 05:00:13 +0100

    feat: add skelenton and delay on requests

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -10,17 +10,18 @@ import {
   DropdownMenuSeparator, 
   DropdownMenuTrigger 
 } from "./ui/dropdown-menu";
+import { Skeleton } from "./ui/skeleton";
 
 import { getProfile } from "@/api/get-profile";
 import { getManagerRestaurant } from "@/api/get-managed-restaurant";
 
 export function AccountMenu() {
-  const { data: profile } = useQuery({
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ['profile'],
     queryFn: getProfile
   })
 
-  const { data: managedRestaurant } = useQuery({
+  const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } = useQuery({
     queryKey: ['managed-restaurant'],
     queryFn: getManagerRestaurant
   })
@@ -29,16 +30,29 @@ export function AccountMenu() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" className="flex items-center gap-2 select-none">
-          {managedRestaurant?.name}
+          {isLoadingManagedRestaurant ? (
+            <Skeleton className="h-4 w-40" />
+          ) : (
+             managedRestaurant?.name
+          )}
           <ChevronDown className="h-4 w-4" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-56">
       <DropdownMenuLabel className="flex flex-col">
-        <span>{profile?.name}</span>
-        <span className="text-xs font-normal text-muted-foreground">
-          {profile?.email}
-        </span>
+        {isLoadingProfile ? (
+          <div className="space-y-1.5">
+            <Skeleton className="h-4 w-32" />
+            <Skeleton className="h-3 w-24" />
+          </div>
+        ) : (
+          <>
+            <span>{profile?.name}</span>
+            <span className="text-xs font-normal text-muted-foreground">
+              {profile?.email}
+            </span>
+          </>
+        )}
       </DropdownMenuLabel>
       <DropdownMenuSeparator />
       <DropdownMenuItem>
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -7,3 +7,14 @@ export const api = axios.create({
   // faz com que os cookies do front-end sejam automaticamente enviado pro backend
   withCredentials: true,
 })
+
+if (env.VITE_ENABLE_API_DELAY) {
+  // antes de todas as requests do axios, chame essa function
+  api.interceptors.request.use(async (config) => {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+    // config: são os dados da requisição, os headers, o body, etc...
+    // Também pode ser usado para customizar os dados da requisição
+    return config
+  })
+}
