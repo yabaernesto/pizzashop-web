578d794b7f1007d7f693924f957f516f8b699c35
Yaba Ernesto <yabaernesto@gmail.com>
2026-03-28 03:10:36 +0100

    feat: configuração do api client com tanstack-query

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
 import { Toaster } from 'sonner'
 import { RouterProvider } from 'react-router-dom'
 import { HelmetProvider, Helmet } from 'react-helmet-async'
+import { QueryClientProvider } from '@tanstack/react-query'
 
 import { ThemeProvider } from '@/components/theme/theme-provider'
 
 import { router } from './routes'
+import { queryClient } from './lib/react-query'
 
 export function App() {
   return (
@@ -12,7 +14,9 @@ export function App() {
       <ThemeProvider defaultTheme='dark' storageKey='pizzashop-theme'>
         <Helmet titleTemplate='%s | pizza.shop'/>
         <Toaster richColors />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
