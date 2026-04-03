343130c084d9b2d940b19f4d2743c6f371ef86dd
Yaba Ernesto <yabaernesto@gmail.com>
2026-04-03 17:42:59 +0100

    feat: filtros dos pedidos adicionado

--- src/pages/app/orders/index.tsx
+++ src/pages/app/orders/index.tsx
@@ -1,7 +1,7 @@
+import { z } from 'zod'
 import { Helmet } from 'react-helmet-async'
 import { useQuery } from '@tanstack/react-query'
 import { useSearchParams } from 'react-router-dom'
-import { z } from 'zod'
 
 import { 
   Table, 
@@ -19,13 +19,21 @@ import { getOrders } from '@/api/get-orders'
 export function Orders() {
   const [searchParams, setSearchParams] = useSearchParams()
 
+  const orderId = searchParams.get('orderId')
+  const customerName = searchParams.get('customerName')
+  const status = searchParams.get('status')
+
   const pageIndex = z.coerce.number()
     .transform((page) => page -1)
     .parse(searchParams.get('page') ?? '1')
 
   const { data: result } = useQuery({
-    queryKey: ['orders', pageIndex],
-    queryFn: () => getOrders({ pageIndex })
+    queryKey: ['orders', pageIndex, orderId, customerName, status],
+    queryFn: () => getOrders({ 
+      pageIndex, 
+      orderId, 
+      customerName, 
+      status: status === 'all' ? null : status })
   })
 
   function handlePaginate(pageIndex: number) {
