b65647aade61337c0d05634ed0d93eb38b9158b4
yabaernesto <yabaernesto@gmail.com>
2026-09-22 21:36:09 +0100

    feat: erros e pagina 404

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { Error } from './pages/error'
+import { NotFound } from './pages/404'
 import { Orders } from './pages/app/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
 import { AppLayout } from './pages/_layout/app'
 import { AuthLayout } from './pages/_layout/auth'
 import { Dashboard } from './pages/app/dashboard'
-import { NotFound } from './pages/404'
 
 export const router = createBrowserRouter([
   { 
     path: '/', element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [
       { path: '/', element: <Dashboard />},
       { path: '/orders', element: <Orders />}
@@ -24,4 +25,8 @@ export const router = createBrowserRouter([
       { path: '/sign-up', element: <SignUp /> },
     ]
   },
+  {
+    path: '*',
+    element: <NotFound />
+  }
 ])
