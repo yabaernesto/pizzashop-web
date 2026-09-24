3b5744430feab9abb5e8b89a6374cc5356bd711c
yabaernesto <yabaernesto@gmail.com>
2026-09-24 15:20:38 +0100

    tests: setup do ambiente de testes

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest/config" />
+
 import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react"
@@ -9,5 +11,13 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    globals: true,
+    setupFiles: ["./test/setup.ts"],
+    coverage: {
+      reporter: ["text", "html"],
+    },
+    environment: "happy-dom",
   },
 });
